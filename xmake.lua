12865ce621ef9dff1d1ac41ac1e19b0770c62f67
K4kug3n <kakugen@yahoo.fr>
2023-06-04 14:14:03 +0200

    Init repo

---
+++ xmake.lua
@@ -0,0 +1,19 @@
+set_project("NotSureYet")
+
+add_requires("sfml", "entt", "nlohmann_json")
+
+add_rules("mode.debug", "mode.release")
+add_rules("plugin.vsxmake.autoupdate")
+
+add_includedirs("include")
+add_headerfiles("include/**.hpp", "include/**.inl")
+
+set_languages("c++17")
+set_warnings("allextra")
+
+
+target("main")
+add_packages("sfml", "entt", "libsdl", "nlohmann_json")
+	add_files("src/**.cpp")
+
+	set_rundir("assets")
