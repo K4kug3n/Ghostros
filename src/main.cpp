07a8f918f4b3edb32eefcd21bc1544262c8ac0dc
K4kug3n <kakugen@yahoo.fr>
2023-07-02 01:15:34 +0200

    Use RenderWindow & input interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 
 #include "Tilemap.hpp"
+#include "Window/RenderWindow.hpp"
+#include "Window/InputHandler.hpp"
 
 int main()
 {
 	TileMap tilemap = TileMap::LoadFromFile("stage_1.json");
 
-	std::cout << "Hello world" << std::endl;
+	RenderWindow window{ 640, 480, "NotSureYet" };
+	InputHandler input_handler;
+
+	input_handler.register_action("quit", Action{ Input::Window::Closed });
+
+	while (window.is_open())
+	{
+		input_handler.update(window);
+
+		if (input_handler.is_active("quit")) {
+			window.close();
+		}
+
+		window.display();
+	}
 
 	return 0;
 }
