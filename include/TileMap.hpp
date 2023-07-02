b70b2ffc9c7cb90d803c315c476301e470d705ca
K4kug3n <kakugen@yahoo.fr>
2023-07-02 19:47:51 +0200

    Add graphic part to TimeMap

--- include/TileMap.hpp
+++ include/TileMap.hpp
@@ -4,19 +4,24 @@
 #include <filesystem>
 #include <vector>
 
+#include <SFML/Graphics/VertexArray.hpp>
+
 #include "TileSet.hpp"
 #include "Maths/Vector2.hpp"
+
+class RenderWindow;
 
 class TileMap
 {
 public:
 	TileMap() = delete;
-	TileMap(unsigned cols, unsigned rows, std::vector<uint8_t> map, TileSet tile_set);
+	TileMap(unsigned cols, unsigned rows, std::vector<uint8_t> indices, TileSet tile_set);
 	TileMap(const TileMap&) = delete;
 	TileMap(TileMap&&) = default;
 	~TileMap() = default;
 
-	uint8_t get_cell_id(int x, int y) const;
+	void draw(RenderWindow& window);
+	uint8_t get_cell_id(unsigned x, unsigned y) const;
 	Vector2i world_to_map(const Vector2i& position) const;
 
 	static TileMap LoadFromFile(const std::filesystem::path& filepath);
@@ -27,8 +32,9 @@ public:
 private:
 	unsigned m_rows;
 	unsigned m_cols;
+	std::vector<uint8_t> m_indices;
 
-	std::vector<uint8_t> m_map;
+	sf::VertexArray m_vertices;
 
 	TileSet m_tile_set;
 };
