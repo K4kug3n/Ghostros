38e1f4117c7a51993540300395e1ec77570ed4af
K4kug3n <kakugen@yahoo.fr>
2023-07-15 11:56:28 +0200

    Fix type of division in Clock

--- src/Utils/Clock.cpp
+++ src/Utils/Clock.cpp
@@ -11,7 +11,7 @@ double Clock::get_elapsed_seconds()
 	std::chrono::duration<double> diff = new_time - m_previous_time;
 	m_previous_time = std::move(new_time);
 
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count() / 1000000000;
+	return std::chrono::duration_cast<std::chrono::nanoseconds>(diff).count() / 1000000000.f;
 }
 
 void Clock::restart()
