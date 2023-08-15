39a59f53e015d2a815bfcdad725a5d4c5deb3045
K4kug3n <kakugen@yahoo.fr>
2023-08-15 23:46:48 +0200

    Simplify sprite cache get() function

--- src/Graphics/TextureCache.cpp
+++ src/Graphics/TextureCache.cpp
@@ -3,22 +3,17 @@
 TextureHandle TextureCache::get(const std::filesystem::path& filepath)
 {
 	std::string key = std::filesystem::absolute(filepath).generic_string();
-	if (m_cache.count(key) == 0)
+	if (m_cache.count(key) != 0)
 	{
-		std::shared_ptr<Texture> texture = std::make_shared<Texture>(filepath);
-		m_cache.insert({ key, texture });
-
-		return TextureHandle{ std::move(texture) };
-	}
-
-	std::weak_ptr<Texture>& weak_texture = m_cache[key];
-	if (auto texture = weak_texture.lock())
-	{
-		return TextureHandle{ std::move(texture) };
+		std::weak_ptr<Texture>& weak_texture = m_cache[key];
+		if (auto texture = weak_texture.lock())
+		{
+			return TextureHandle{ std::move(texture) };
+		}
 	}
 
 	std::shared_ptr<Texture> texture = std::make_shared<Texture>(filepath);
-	weak_texture = texture;
+	m_cache[key] = texture;
 
 	return TextureHandle{ std::move(texture) };
 }
