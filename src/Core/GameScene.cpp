6ab9021f75dc2e9ebacff9e5e4f975b1387c49b5
K4kug3n <kakugen@yahoo.fr>
2023-08-31 01:25:58 +0200

    Move init scene to load function

--- src/Core/GameScene.cpp
+++ src/Core/GameScene.cpp
@@ -1,7 +1,81 @@
 #include "Core/GameScene.hpp"
 
+#include "Window/Components/Input.hpp"
+
+#include "Physics/Systems/PhysicsSpace.hpp"
+#include "Physics/Components/StaticBody.hpp"
+#include "Physics/Components/RigidBody.hpp"
+#include "Physics/Components/Collision.hpp"
+
+#include "ECS/Systems/CollisionTrigger.hpp"
+#include "ECS/Systems/PlayerSystem.hpp"
+#include "ECS/Components/Node.hpp"
+#include "ECS/Components/State.hpp"
+#include "ECS/Components/CollisionCallback.hpp"
+#include "ECS/EntityHandler.hpp"
+
+#include "Graphics/Systems/AnimationSystem.hpp"
+#include "Graphics/Systems/StateAnimationSystem.hpp"
+#include "Graphics/Components/StateAnimation.hpp"
+#include "Graphics/Components/Sprite.hpp"
+#include "Graphics/Components/TileMap.hpp"
+#include "Graphics/Components/Camera.hpp"
+#include "Graphics/TextureCache.hpp"
+
 void GameScene::load()
 {
+	PhysicsSpace& space = world.add_system<PhysicsSpace>();
+	space.set_gravity({ 0.f, 98.1f });
+
+	world.add_system<CollisionTrigger>();
+	world.add_system<AnimationSystem>();
+	world.add_system<StateAnimationSystem>();
+	world.add_system<PlayerSystem>();
+
+	EntityHandler player = world.create();
+
+	TextureCache texture_cache;
+
+	StateAnimation& state_animation = player.add_component<StateAnimation>();
+	TextureHandle right_texture = texture_cache.get("sprites/right_ghost.png");
+	state_animation.add_animation(State::RUN_RIGHT, { right_texture->get_size(), Vector2u{ 3u, 1u }, 3.f, AnimationRepetition::REPEAT }, right_texture);
+
+	TextureHandle idle_texture = texture_cache.get("sprites/idle_ghost.png");
+	state_animation.add_animation(State::IDLE, { idle_texture->get_size(), Vector2u{ 3u, 1u }, 3.f, AnimationRepetition::REPEAT }, idle_texture);
+
+	TextureHandle left_texture = texture_cache.get("sprites/left_ghost.png");
+	state_animation.add_animation(State::RUN_LEFT, { left_texture->get_size(), Vector2u{ 3u, 1u }, 3.f, AnimationRepetition::REPEAT }, left_texture);
+	player.add_component<Sprite>(std::move(left_texture));
+
+	player.add_component<Node>(Vector3f{ 96.f, 96.f, 0.f }, Vector2f{ 27.f, 29.f });
+	player.add_component<RigidBody>();
+	player.add_component<Input>(Input::NONE);
+	player.add_component<State>(State::IDLE);
+
+	EntityHandler tilemap = world.create();
+	tilemap.add_component<TileMap>(TileMap::LoadFromFile("stage_1.json"));
+	tilemap.add_component<Node>(Vector3f{ 0.f, 0.f, 0.f }, Vector2{ 0.f, 0.f });
+
+	EntityHandler win_trigger = world.create();
+	win_trigger.add_component<Node>(Vector3f{ 1724.f, 576.f, 0.f }, Vector2f{ 69.f, 288.f });
+	win_trigger.add_flag<StaticBody>();
+	TextureHandle flag_texture = texture_cache.get("sprites/flag.png");
+	Sprite& flag_sprite = win_trigger.add_component<Sprite>(flag_texture);
+	flag_sprite.set_texture_rect({ 0, 0 }, { 23, 96 });
+	win_trigger.add_component<CollisionCallback>(
+		[flag_texture](const EntityHandler& self, const EntityHandler& other, const Vector2f&)
+		{
+			other.remove<Input>();
+			other.get<State>() = State::IDLE;
+
+			self.remove<StaticBody>();
+			self.remove<Collision>();
+
+			self.add_component<Animation>(flag_texture->get_size(), Vector2u{ 5u, 1u }, 1.f);
+		}
+	);
+
+	Camera& camera = player.add_component<Camera>(0, 0, 640, 480);
 }
 
 void GameScene::unload()
--- src/main.cpp
+++ src/main.cpp
@@ -33,62 +33,12 @@
 int main()
 {
 	GameScene game_scene;
+	game_scene.load();
 
 	World& world = game_scene.world;
-	PhysicsSpace& space = world.add_system<PhysicsSpace>();
-	space.set_gravity({ 0.f, 98.1f });
-
-	world.add_system<CollisionTrigger>();
-	world.add_system<AnimationSystem>();
-	world.add_system<StateAnimationSystem>();
-	world.add_system<PlayerSystem>();
-
-	EntityHandler player = world.create();
-
-	TextureCache texture_cache;
-
-	StateAnimation& state_animation = player.add_component<StateAnimation>();
-	TextureHandle right_texture = texture_cache.get("sprites/right_ghost.png");
-	state_animation.add_animation(State::RUN_RIGHT, { right_texture->get_size(), Vector2u{ 3u, 1u }, 3.f, AnimationRepetition::REPEAT }, right_texture);
-
-	TextureHandle idle_texture = texture_cache.get("sprites/idle_ghost.png");
-	state_animation.add_animation(State::IDLE, { idle_texture->get_size(), Vector2u{ 3u, 1u }, 3.f, AnimationRepetition::REPEAT }, idle_texture);
-
-	TextureHandle left_texture = texture_cache.get("sprites/left_ghost.png");
-	state_animation.add_animation(State::RUN_LEFT, { left_texture->get_size(), Vector2u{ 3u, 1u }, 3.f, AnimationRepetition::REPEAT }, left_texture);
-	player.add_component<Sprite>(std::move(left_texture));
-
-	player.add_component<Node>(Vector3f{ 96.f, 96.f, 0.f }, Vector2f{ 27.f, 29.f });
-	player.add_component<RigidBody>();
-	player.add_component<Input>(Input::NONE);
-	player.add_component<State>(State::IDLE);
-
-	EntityHandler tilemap = world.create();
-	tilemap.add_component<TileMap>(TileMap::LoadFromFile("stage_1.json"));
-	tilemap.add_component<Node>(Vector3f{ 0.f, 0.f, 0.f }, Vector2{ 0.f, 0.f });
-
-	EntityHandler win_trigger = world.create();
-	win_trigger.add_component<Node>(Vector3f{ 1724.f, 576.f, 0.f }, Vector2f{ 69.f, 288.f });
-	win_trigger.add_flag<StaticBody>();
-	TextureHandle flag_texture = texture_cache.get("sprites/flag.png");
-	Sprite& flag_sprite = win_trigger.add_component<Sprite>(flag_texture);
-	flag_sprite.set_texture_rect({ 0, 0 }, { 23, 96 });
-	win_trigger.add_component<CollisionCallback>(
-		[flag_texture](const EntityHandler& self, const EntityHandler& other, const Vector2f&)
-		{
-			other.remove<Input>();
-			other.get<State>() = State::IDLE;
-
-			self.remove<StaticBody>();
-			self.remove<Collision>();
-
-			self.add_component<Animation>(flag_texture->get_size(), Vector2u{ 5u, 1u }, 1.f);
-		}
-	);
 	
 	RenderWindow window{ 640, 480, "Ghostros" };
 	world.add_system<RenderSystem>(window);
-	Camera& camera = player.add_component<Camera>(0, 0, window.get_size().x, window.get_size().y);
 
 	InputHandler input_handler;
 	input_handler.register_action("quit", Action{ InputEvent::Window::Closed });
@@ -111,7 +61,7 @@ int main()
 		}
 		if (input_handler.is_active("resize"))
 		{
-			camera.set_size(window.get_size());
+			//camera.set_size(window.get_size());
 		}
 
 		window.clear();
